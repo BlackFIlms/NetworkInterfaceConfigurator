cf0734e59949d08d53441cbe708805c09dd3abd2
Matvey <blackfilms13@gmail.com>
2020-02-28 00:02:17 +0500

    Create project

--- NetworkInterfaceTest/NetworkInterfaceTest.cs
+++ NetworkInterfaceTest/NetworkInterfaceTest.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using Microsoft.Win32;
 
 namespace NetworkInterfaceTest
 {
-    class Program
+    class NetworkInterfaceTest
     {
+        public static string output;
+
         static void Main(string[] args)
         {
+            RegistryKey key;
+            output = "1";
         }
     }
 }
