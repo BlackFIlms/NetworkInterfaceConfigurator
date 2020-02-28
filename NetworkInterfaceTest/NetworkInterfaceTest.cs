64927e76a2b251eba9b1047e95983d56d2166106
BlackFilms <blackfilms13@gmail.com>
2020-02-29 02:03:42 +0500

    Designed methods for Get ID and name network adapters from Registry. And designed  method for Get the Networks Name, from Registry too. And all logic  structures moved to class Adapters. And all adapters will be objects of this class. Added more comments and cool XML descriptions.

--- NetworkInterfaceTest/NetworkInterfaceTest.cs
+++ NetworkInterfaceTest/NetworkInterfaceTest.cs
@@ -11,21 +11,30 @@ namespace NetworkInterfaceTest
 {
     class NetworkInterfaceTest
     {
-        private const string baseReg = @"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\";
-        public static string output;
+        //Defining variables
+        public static List<Adapter> adapters = new List<Adapter>();
 
         static void Main(string[] args)
         {
-            string bkey = GetBaseKey().ToString();
-            string check64 = InternalCheckIsWow64().ToString();
+            //It's validation check.
+            string bkey = Adapter.GetBaseKey().ToString();
+            string check64 = Adapter.InternalCheckIsWow64().ToString();
 
-            string adapters = "";
-            foreach (string adapter in GetNicIds())
+            //Get adapters list with ID's, NIC names and Network names.
+            /*string adapters = "";
+            foreach (string adapter in Adapter.GetNicIds())
             {
                 adapters += adapter + "\r\n";
+            }*/
+            
+            foreach (string adapter in Adapter.GetNicIds())
+            {
+                string regKey;
+                adapters.Add(new Adapter() { NicID = adapter, NicName = Adapter.GetNicName(adapter, out regKey), NetName = Adapter.GetNetName(regKey) });
             }
 
-            output = bkey + " $$$ " + check64;
+            //Console out.
+            string output = bkey + " $$$ " + check64;
             Console.WriteLine(output);
             Console.WriteLine("Do you want see adapter list? y/n");
             Console.Beep();
@@ -34,7 +43,13 @@ namespace NetworkInterfaceTest
             switch (conKey1)
             {
                 case 'y':
-                    Console.WriteLine(adapters);
+                    foreach (Adapter item in adapters)
+                    {
+                        Console.WriteLine("Network adapter ID in Registry: " + item.NicID + "\r\n");
+                        Console.WriteLine("Network adapter name: " + item.NicName + "\r\n");
+                        Console.WriteLine("Network name: " + item.NetName + "\r\n");
+                        Console.WriteLine("\r\n");
+                    }
                     Console.ReadKey();
                     break;
                 case 'n':
@@ -43,13 +58,37 @@ namespace NetworkInterfaceTest
                     break;
             }
         }
+    }
+    class Adapter
+    {
+        //Defining variables for objects.
+        /// <summary>
+        /// Get or Set Network adapter id.
+        /// </summary>
+        public string NicID { get; set; }
+        /// <summary>
+        /// Get or Set Network adapter Name.
+        /// </summary>
+        public string NicName { get; set; }
+        /// <summary>
+        /// Get or Set Network name.
+        /// </summary>
+        public string NetName { get; set; }
 
-        //Set new MAC addres.
+        //Set constants for Rgistry.
+        private const string netAdaptersReg = @"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\";
+        private const string netConnNamesReg = @"SYSTEM\ControlSet001\Control\Network\{4D36E972-E325-11CE-BFC1-08002BE10318}\";
+        
+        //Methods.
+
+        /// </summary>
+        /// Set new MAC addres.
+        /// </summary>
         public static bool SetMAC(string nicID, string newMAC)
         {
             bool ret = false;
             using (RegistryKey bkey = GetBaseKey())
-            using (RegistryKey key = bkey.OpenSubKey(baseReg + nicID))
+            using (RegistryKey key = bkey.OpenSubKey(netAdaptersReg + nicID))
             {
                 if (key != null)
                 {
@@ -69,18 +108,13 @@ namespace NetworkInterfaceTest
 
             return ret;
         }
-        //Get adapter name from registry.
-        public static IEnumerable<string> GetNicName(string nicID)
-        {
-            using (RegistryKey bkey = GetBaseKey())
-            using (RegistryKey key = bkey.OpenSubKey(baseReg + nicID))
-                return;
-        }
-        //Get physical adapter IDs from registry.
+        /// <summary>
+        /// Get physical adapter IDs from registry.
+        /// </summary>
         public static IEnumerable<string> GetNicIds()
         {
             using (RegistryKey bkey = GetBaseKey())
-            using (RegistryKey key = bkey.OpenSubKey(baseReg))
+            using (RegistryKey key = bkey.OpenSubKey(netAdaptersReg))
             {
                 if (key != null)
                 {
@@ -106,8 +140,47 @@ namespace NetworkInterfaceTest
                 }
             }
         }
+        /// <summary>
+        /// Get adapter name from registry and it can output registry branch for it's adapter.
+        /// </summary>
+        public static string GetNicName(string nicID, out string regKey)
+        {
+            using (RegistryKey bkey = GetBaseKey())
+            using (RegistryKey key = bkey.OpenSubKey(netAdaptersReg + nicID))
+                if (key != null)
+                {
+                    regKey = key.GetValue("NetCfgInstanceId").ToString();
+                    string name = key.GetValue("DriverDesc").ToString();
+                    return name;
+                }
+                else
+                {
+                    regKey = null;
+                    return "Something with registry."; //if we can't find registry branch, we send this string for Error.
+                }
+        }
+        ///  <summary>
+        ///  Get network name from registry.
+        ///  </summary>
+        public static string GetNetName(string regKey)
+        {
+            using (RegistryKey bkey = GetBaseKey())
+            using (RegistryKey key = bkey.OpenSubKey(netConnNamesReg + regKey + @"\Connection"))
+                if (key != null)
+                {
+                    string name = key.GetValue("Name").ToString();
+                    return name;
+                }
+                else
+                {
+                    regKey = null;
+                    return "Something with registry."; //if we can't find registry branch, we send this string for Error.
+                }
+        }
 
-        //This functions check system version (64/32bit) and then get corresponding branch of registry.
+        /// <summary>
+        /// This functions check system version (64/32bit) and then get corresponding branch of registry.
+        /// </summary>
         public static RegistryKey GetBaseKey()
         {
             return RegistryKey.OpenBaseKey(
