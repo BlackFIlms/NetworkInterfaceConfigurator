857302f7b3f8e3d61c177119675a5f4fb0974024
BlackFilms <blackfilms13@gmail.com>
2020-02-28 17:29:18 +0500

    Add System bits check.

--- NetworkInterfaceTest/NetworkInterfaceTest.cs
+++ NetworkInterfaceTest/NetworkInterfaceTest.cs
@@ -14,8 +14,26 @@ namespace NetworkInterfaceTest
 
         static void Main(string[] args)
         {
-            RegistryKey key;
-            output = "1";
+            string bkey = GetBaseKey().ToString();
+            string check64 = InternalCheckIsWow64().ToString();
+
+            output = bkey + " $$$ " + check64;
+            Console.WriteLine(output);
+            Console.Beep();
+            Console.ReadKey();
+        }
+
+        public static RegistryKey GetBaseKey()
+        {
+            return RegistryKey.OpenBaseKey(
+                RegistryHive.LocalMachine,
+                InternalCheckIsWow64() ? RegistryView.Registry64 : RegistryView.Registry32);
+        }
+
+        public static bool InternalCheckIsWow64()
+        {
+            bool b = Environment.Is64BitOperatingSystem;
+            return b;
         }
     }
 }
