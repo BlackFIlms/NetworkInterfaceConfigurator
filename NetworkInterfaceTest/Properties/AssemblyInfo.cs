9b018540a416977a19b2a7789cae59e71c71f458
BlackFilms <blackfilms13@gmail.com>
2020-03-01 23:03:15 +0500

    Change version info.

--- NetworkInterfaceTest/Properties/AssemblyInfo.cs
+++ NetworkInterfaceTest/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("NetworkInterfaceTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Designed by AvdeevMatvey")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BlackFilms")]
 [assembly: AssemblyProduct("NetworkInterfaceTest")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("BlackFilms ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
