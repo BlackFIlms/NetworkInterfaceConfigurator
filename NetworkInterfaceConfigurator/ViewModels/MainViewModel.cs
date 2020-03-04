b44742a267cc580d7a55fadc5fb312f6eb4ea00c
BlackFilms <blackfilms13@gmail.com>
2020-03-05 01:07:46 +0500

    Created layout of the design. Start designing UI with MVVM. Splitted the BaseGrid on Subgrids.

---
+++ NetworkInterfaceConfigurator/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DevExpress.Mvvm;
+
+namespace NetworkInterfaceConfigurator.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        private int _Clicks;
+        
+        public int Clicks
+        {
+            get { return _Clicks; }
+
+            set
+            {
+                _Clicks = value;
+                RaisePropertyChanged(() => Clicks);
+            }
+        }
+
+
+        public ICommand ClickAdd
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    Clicks++;
+                });
+            }
+        }
+    }
+}
